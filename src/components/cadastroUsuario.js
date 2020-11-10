0eb598e37a6cd4b0e82364bad28096359c277e6f
ThiagoCantanhede <thiago.canta@hotmail.com>
2020-11-09 22:37:54 -0300

    ajuste de layout cadastro de usuário

--- src/components/cadastroUsuario.js
+++ src/components/cadastroUsuario.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import operacoes from '../services/LigadosService.js';
 export default function CadastroUsuario(props) {
-  const imprime = () => {
+  const salvarUsuario = () => {
     console.log('teste');
     var usuario = {
       nome: 'usuário do frontend',
@@ -12,18 +12,46 @@ export default function CadastroUsuario(props) {
     operacoes.create(usuario);
   };
   return (
-    <div className="row">
+    <div className="row container">
       <form className="col s12">
-        <div className="row">
-          <div className="input-field col s12">
+        <div className="row container">
+          <div className="input-field col s12"></div>
+          <div className="input-field col s4"></div>
+          <div className="input-field col s8">
             <input id="nome" type="text" className="validate"></input>
             <label className="active" htmlFor="nome">
-              Nome
+              Nome completo
             </label>
           </div>
-          <a className="waves-effect waves-light btn" onClick={imprime}>
-            Salvar
-          </a>
+
+          <div className="input-field col s4"></div>
+          <div className="input-field col s8">
+            <input id="nome" type="text" className="validate"></input>
+            <label className="active" htmlFor="nome">
+              Nome de usuário
+            </label>
+          </div>
+
+          <div className="input-field col s4"></div>
+          <div className="input-field col s8">
+            <input id="nome" type="text" className="validate"></input>
+            <label className="active" htmlFor="nome">
+              E-mail
+            </label>
+          </div>
+          <div className="input-field col s4"></div>
+          <div className="input-field col s8">
+            <input id="nome" type="text" className="validate"></input>
+            <label className="active" htmlFor="nome">
+              Senha
+            </label>
+          </div>
+          <div className="input-field col s4"></div>
+          <div className="input-field col s8">
+            <a className="waves-effect waves-light btn" onClick={salvarUsuario}>
+              Salvar
+            </a>
+          </div>
         </div>
       </form>
     </div>
