0180d1164ee9dc97fd5e6d12efbad8333be572ad
ThiagoCantanhede <thiago.canta@hotmail.com>
2021-02-04 00:48:54 -0300

    ajuste

--- src/app.js
+++ src/app.js
@@ -3,37 +3,6 @@ import { Link } from 'react-router-dom';
 
 export default function App(props) {
   return (
-    // <div className="navbar-fixed">
-    //   <nav className="blue " role="navigation">
-    //     <div className="container">
-    //       <div className="nav-wrapper">
-    //         <ul id="nav-mobile">
-    //           <li>
-    //             <Link to="/cadastro">Cadastrar usuário</Link>
-    //           </li>
-    //           <li>
-    //             <Link to="/curriculo">Cadastrar currículo</Link>
-    //           </li>
-    //           <li>
-    //             <Link to="/consultarCurriculos">Listagem de currículos</Link>
-    //           </li>
-    //           <li>
-    //             <Link to="/vaga">Cadastrar vaga</Link>
-    //           </li>
-    //           <li>
-    //             <Link to="/consultarVagas">Listagem de vagas</Link>
-    //           </li>
-    //           <li>
-    //             <Link to="/escreverArtigo">Escrever artigo</Link>
-    //           </li>
-    //           <li>
-    //             <Link to="/consultarArtigos">Listagem de artigos</Link>
-    //           </li>
-    //         </ul>
-    //       </div>
-    //     </div>
-    //   </nav>
-    // </div>
     <div className="row container">
       <div className="col s12 m4">
         <div className="card blue-grey darken-1">
