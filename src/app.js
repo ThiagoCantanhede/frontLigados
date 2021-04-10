ed5fe141f3663c4ee8a5e4a657d9d80264479210
ThiagoCantanhede <thiago.canta@hotmail.com>
2021-04-10 12:01:22 -0300

    Mostrara listagem de vagas apenas para o usuário candidato

--- src/app.js
+++ src/app.js
@@ -72,7 +72,12 @@ export default function App(props) {
         </div>
       </div>
 
-      <div className="col s12 m4">
+      <div
+        style={{
+          display: retornarTipoUsuario() != 'Recrutador' ? 'block' : 'none',
+        }}
+        className="col s12 m4"
+      >
         <div className="card blue-grey darken-1">
           <div className="card-content white-text">
             <span className="card-title">
